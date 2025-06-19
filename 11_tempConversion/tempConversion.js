36a69fb82d95e4fdb9a9b7123ea4250394fd513e
dennisou0417 <dennis.ou0417@gmail.com>
2025-06-19 00:08:56 -0400

    tempConversions finished and all tests passed

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah) {
+  let cel = (fah - 32) / 1.8
+  return Math.round(cel * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let fah = (cel * 1.8) + 32
+  return Math.round(fah * 10)/ 10;
 };
 
 // Do not edit below this line
