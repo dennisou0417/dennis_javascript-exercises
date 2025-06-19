0bf13a2ea695fd7e3fe54370acc9e861eb822264
dennisou0417 <dennis.ou0417@gmail.com>
2025-06-18 23:17:14 -0400

    removeFromArray function finished and tests

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...items) {
+    let newArray = [];
+
+    arr.forEach((item) => {
+        if(!items.includes(item)){
+            newArray.push(item);
+        }
+    })
+
+    return newArray;
 };
 
 // Do not edit below this line
